1738cc37aa2a753c22aad022b73c86204c454d26
Alexander Cherry <acherry125@gmail.com>
2018-03-01 00:08:23 -0500

    These first lines of scope are all the same rule

--- rsl-scope.rkt
+++ rsl-scope.rkt
@@ -3,14 +3,7 @@
 
 -----------RSL Core Scope-----------:
 
-(define original-ds (DataShell-import 'a "data.csv")) binds original-ds in (DataShell-import 'a "data.csv")
-
-(define map-ds (map expr original-ds)) binds map-ds in (map expr original-ds)
-(define reduce-map-ds (reduce expr map-ds)) binds reduce-map-ds in (reduce expr map-ds)
-
-(define filter-map-ds (filter expr map-ds)) binds filter-map-ds in (filter expr map-ds)
-(define collect-filter-map-ds (collect expr filter-map-ds))
-binds collect-filter-map-ds in (collect expr filter-map-ds)
+(define x e) binds x in e
 
 (λ (x) e) binds x in e
 
