58124ce360c796a853018d8e024cf2cc5d4f253c
Alexander Cherry <acherry125@gmail.com>
2018-02-28 23:58:11 -0500

    Restricted our  inputs to only be our specified function signatures

--- rsl-grammar.rkt
+++ rsl-grammar.rkt
@@ -8,38 +8,36 @@ Program		=	top-level
 
 Top-level       =       Definition
                 |       TExpr
+                |       Expr (from Racket)
 
-Definition	= 	(define Id Expr)
+Definition	= 	(define Id TExpr)
 
-
-TExpr		=	DataShell
+TExpr		=       DataShell
 		=	Tranformation
 		=	Action
-		=	(λ (e) e)
-		=	(λ (e) w)
-		=	(λ (e loe) w)
-		=	All Racket expressions
 
+TFunc		=	(λ (x) Expr)
+		=	(λ (x) Expr)
+
+AFunc		=	(λ (x1 x2) Expr)
 
 DataShell	= 	(DataShell-import Id ref)
-		= 	(DataShell Id Tranformation)
-		= 	(DataShell Id Action)
+		= 	(DataShell x)
+		= 	(DataShell x Action)
 
+Tranformation   =       (ds-map TExpr DataShell)
+		= 	(ds-map TFunc Tranformation) 
+		= 	(ds-filter TFunc DataShell)
+		= 	(ds-filter TFunc Tranformation)
+		= 	(ds-flatmap TFunc DataShell)
+		= 	(ds-flatmap TFunc Tranformation)
 
-Tranformation	= 	(map Expr DataShell)
-		= 	(map Expr Tranformation)
-		= 	(filter Expr DataShell)
-		= 	(filter Expr Tranformation)
-		= 	(flatmap Expr DataShell)
-		= 	(flatmap Expr Tranformation)
-
-
-Action		= 	(reduce Expr DataShell)
-		= 	(reduce Expr Transformation)
-		= 	(collect DataShell)
-		= 	(collect Transformation)
-		= 	(count DataShell)
-		= 	(count Transformation)
+Action		= 	(ds-reduce AFunc DataShell)
+		= 	(ds-reduce AFunc Transformation)
+		= 	(ds-collect DataShell)
+		= 	(ds-collect Transformation)
+		= 	(ds-count DataShell)
+		= 	(ds-count Transformation)
 
 Examples:
 (DataShell 'result (reduce Expr (DataShell 'b (map Expr (DataShell-import 'a "data.csv")))))
