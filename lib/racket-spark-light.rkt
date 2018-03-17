f8eeca224a66d81594de7396a56b853b9dad8f70
Alexander Cherry <acherry125@gmail.com>
2018-03-17 12:23:27 -0400

    library, implemented ds-map, implementing ds-reduce

--- lib/racket-spark-light.rkt
+++ lib/racket-spark-light.rkt
@@ -9,16 +9,17 @@
  (except-out (all-from-out racket)
              lambda)
  (rename-out [rsl-lambda lambda])
- tagged-lambda
  ds-map
- mk-datashell)
+ ds-reduce
+ mk-datashell
+ collect-ds)
 
 ;; ---------------------------------------------------------------------------------------------------
 ;; DEPENDENCY
 
 (require (for-syntax syntax/parse
                      syntax/kerncase))
-(require ;;graph
+(require ;; graph
          racket/struct
          (prefix-in un: racket))
 
@@ -37,21 +38,20 @@
 ;; can be used to determine if it is a tfunc or afunc suitable for RSL MapReduce.
 (define-syntax rsl-lambda
   (syntax-parser
+    ;; TFunc, add the syntax property to mark this function as a TFunc
     [(_ (arg1:id) e1:expr e2:expr ...)
      #:with func #'(lambda (arg1) e1 e2 ...)
      #:with tagged (syntax-property #'func 'rsl-func-type 'tfunc)
      #'tagged]
+    ;; AFunc, add the syntax property to mark this function as an AFunc
     [(_ (arg1:id arg2:id) e1:expr e2:expr ...)
      #:with func #'(lambda (arg1 arg2) e1 e2 ...)
      (syntax-property #'func 'rsl-func-type 'afunc)]
+    ;; Regular lambda, not compatible with RSL
     [(_ e ...)
      (displayln "case 3")
      #'(lambda e ...)]))
 
-(define-syntax (tagged-lambda stx)
-  (syntax-parse stx
-    [(_ e)
-     (syntax-property #'e 'rsl-func-type 'tfunc)]))
 
 (define-syntax ds-map
   (syntax-parser
@@ -59,8 +59,8 @@
     [(_ f ds)
      #:with exp (local-expand #'f (syntax-local-context) (kernel-form-identifier-list))
      #:with tfunc (syntax-property #'exp 'rsl-func-type)
-     ;; TODO do something here
-     #'(Datashell (Datashell-dataset ds) (Datashell-ops ds) empty)]))
+     ;; cons the new function to the old queue of functions
+     #'(Datashell (Datashell-dataset ds) (compose f (Datashell-ops ds)))]))
 
 (define-syntax ds-reduce
   (syntax-parser
@@ -68,17 +68,25 @@
     [(_ f ds)
      #:with exp (local-expand #'f (syntax-local-context) (kernel-form-identifier-list))
      #:with afunc (syntax-property #'exp 'rsl-func-type)
-     ;; TODO do something here
-     #'(Datashell (Datashell-dataset ds) (Datashell-ops ds) empty)]))
+     ;; perform the queued transformations, then run the reduction
+     #'(foldl f (ds))]))
+
+
 
 ;; -----------------------------------------------------------------------------
 ;; RUNTIME LIB
-
-(struct Datashell (dataset ops rest) #:transparent)
 
 ;; [Listof Any] -> Datashell
 ;; Create a Datashell from a given list
 (define (mk-datashell data-list)
   (if (list? data-list)
-      (Datashell data-list empty empty)
+      (Datashell data-list void)
       (error 'mk-datashell "First arg must be a list")))
+
+(define (collect-ds this)
+  ;; the thing we want to do when we use the DataSet as a function
+  ;; apply composed function to the list
+  (define mapped (map (Datashell-ops this) (Datashell-dataset this)))
+  mapped)
+
+(struct Datashell (dataset ops) #:transparent #:property prop:procedure collect-ds)
--- test/tests.rkt
+++ test/tests.rkt
@@ -1,7 +1,12 @@
 #lang s-exp "../lib/racket-spark-light.rkt"
 (define x (lambda (z) 3))
 
-
 ((lambda (z) 3) 4)
 
-(ds-map (lambda (r) r) (mk-datashell '(1 3)))
+(ds-reduce (lambda (r acc) (+ r acc))
+           (ds-map (lambda (y) (* 2 y))
+                   (ds-map (lambda (z) (add1 z))
+                           (mk-datashell '(1 3)))))
+
+(define xx (mk-datashell '(12 2)))
+(xx)
