e160e904485746db5d44c854bc41a07c482dfda8
Alexander Cherry <acherry125@gmail.com>
2018-03-18 23:53:09 -0400

    implemented save-ds, comments on other functions, error cases

--- lib/racket-spark-light.rkt
+++ lib/racket-spark-light.rkt
@@ -7,12 +7,30 @@
 (provide
  ;; We want to provide the full racket langauge
  (except-out (all-from-out racket)
-             lambda)
- (rename-out [rsl-lambda lambda])
+             lambda
+             #%module-begin)
+ (rename-out [rsl-lambda lambda]
+             [rsl-module-begin #%module-begin])
+ 
+ ;; ds-map: TFUNC Datashell -> Datashell
+ ;; Creates a new Datashell with the old Datashell mapped with the given function.
  ds-map
+ 
+ ;; ds-reduce: AFUNC Any Datashell -> Any
+ ;; Collects the data in a Datashell, then reduces it with the given function and accumulator.
  ds-reduce
+ 
+ ;; mk-datashell: [Listof Any] -> Datashell
+ ;; Creates a Datashell data structure from a list of items.
  mk-datashell
- ds-collect)
+ 
+ ;; ds-collect: Datashell -> [Listof Any]
+ ;; Collects the data in a Datashell and returns it.
+ ds-collect
+ 
+ ;; save-ds: Id Datashell -> Void
+ ;; EFFECTS: Binds the Datashell to the given identifier in the global scope. Must be used at the top-level.
+ save-ds)
 
 ;; ---------------------------------------------------------------------------------------------------
 ;; DEPENDENCY
@@ -20,18 +38,21 @@
 (require (for-syntax syntax/parse
                      syntax/kerncase))
 (require ;; graph
-         racket/struct
-         (prefix-in un: racket))
+  racket/struct
+  (prefix-in un: racket))
 
 (module+ test (require rackunit))
-
-
 
 ;; ---------------------------------------------------------------------------------------------------
 ;; IMPLEMENTATION
 
 ;; -----------------------------------------------------------------------------
 ;; SYNTAX (compile time functions)
+
+(define-syntax rsl-module-begin
+  (syntax-parser
+    [(_ e ...)
+     #'(#%module-begin (validate-top-level e) ...)]))
 
 ;; (rsl-lambda (arg ...) body body-more ...)
 ;; RSL lambda is currently regular lambda, may not be needed
@@ -41,11 +62,27 @@
     [(_ e ...)
      #'(lambda e ...)]))
 
-; ds-map: (ds-map TFunc Datashell)
-; maps the given function on the Datashell and returns a new Datashell
-; Transformation: lazily evaluated. Compose the given function with the previous functions
-; but do not evaluated the given function
-; Example: (ds-map (lambda (x) (+ 1 x)) (mk-datashell '(1 2)) -> (Datashell '2 3)
+;; validate-top-level: Syntax -> Syntax
+;; Validates top-level save-ds's
+(define-syntax validate-top-level
+  (syntax-parser
+    #:literals (save-ds)
+    [(_ (save-ds e ...))
+     #'(save-ds-top e ...)]
+    [(_ e)
+     (define f (local-expand #'e (syntax-local-context) #f))
+     (syntax-parse f 
+       #:literals (save-ds)
+       [(save-ds args ...)
+        #'(save-ds-top args ...)]
+       [other
+        #'other])]))
+
+;; ds-map: (ds-map TFunc Datashell)
+;; maps the given function on the Datashell and returns a new Datashell
+;; Transformation: lazily evaluated. Compose the given function with the previous functions
+;; but do not evaluated the given function
+;; Example: (ds-map (lambda (x) (+ 1 x)) (mk-datashell '(1 2)) -> (Datashell '2 3)
 (define-syntax (ds-map stx)
   (syntax-parse stx
     #:literals (rsl-lambda)
@@ -53,16 +90,16 @@
     [(_ (lambda (args ...) body ...) ds)
      #:with l (length (syntax->list #'(args ...)))
      #:fail-unless (= (eval #'l) 1) "lambda must have 1 argument in ds-map"
+     ;; reconstruct lambda and pass to the runtime function
      #'(ds-map-func (lambda (args ...) body ...) ds)]
     [(_ f:id ds)
+     ;; pass to the runtime function
      #'(ds-map-func f ds)]))
 
-
-
-; ds-reduce: AFunc Expr Datashell -> Any
-; reduces the Datashell to a non Datashell type
-; Action: eagerly evaluated, triggers all transformations stored on the datashell and the actor
-; apply the final composed function on the given datashell and return the result
+;; ds-reduce: AFunc Any Datashell -> Any
+;; reduces the Datashell to a non Datashell type
+;; Action: eagerly evaluated, triggers all transformations stored on the datashell and the actor
+;; apply the final composed function on the given datashell and return the result
 (define-syntax ds-reduce
   (syntax-parser
     #:literals (rsl-lambda)
@@ -70,10 +107,36 @@
     [(_ (lambda (args ...) body ...) acc:expr ds)
      #:with l (length (syntax->list #'(args ...)))
      #:fail-unless (= (eval #'l) 2) "lambda must have 2 arguments in ds-reduce"
+     ;; reconstruct the lambda, pass to the runtime function
      #'(ds-reduce-func (lambda (args ...) body ...) acc ds)]
     [(_ f acc ds)
-     ;; perform the queued transformations, then run the reduction
+     ;; pass to the runtime function
      #'(ds-reduce-func f acc ds)]))
+
+;; save-ds: Id Datashell -> Void
+;; EFFECTS: Always throws an error, see save-ds-top for the valid macro
+(define-syntax save-ds
+  (syntax-parser
+    [(_ e ...)
+     #:fail-unless #f "save-ds may only be used at the top level"
+     #'(error 'save-ds "save-ds may only be used at the top level")]))
+
+;; save-ds-top: x Datashell -> Void
+;; EFFECTS: Binds the Datashell to the given identifier in the global scope. Must be used at the top-level.
+(define-syntax save-ds-top
+  (syntax-parser
+    #:literals (mk-datashell ds-map ds-reduce)
+    [(_ i:id (mk-datashell e ...))
+     #'(begin (define i (mk-datashell e ...)))]
+    [(_ i:id (ds-map e ...))
+     #'(begin (define i (ds-map e ...)))]
+    [(_ i:id e)
+     #'(begin (define i e)
+              (unless (Datashell? e)
+                ;; TODO better error, make it point to this place
+                (error 'save-ds "requires an identifier and a Datashell as arguments")))]
+    [(_ e ...)
+     #'(error 'save-ds "save-ds requires an identifier and a Datashell as arguments")]))
 
 ;; -----------------------------------------------------------------------------
 ;; RUNTIME LIB
@@ -85,7 +148,8 @@
       (Datashell data-list (lambda (any) any))
       (error 'mk-datashell "First arg must be a list")))
 
-;; collect-ds: Datashell -> [Listof Any]
+;; ds-collect: Datashell -> [Listof Any]
+;; Collects the data in a Datashell and returns it.
 (define (ds-collect ds)
   ;; apply composed function to the stored list, then return the list
   (define mapped (map (Datashell-op ds) (Datashell-dataset ds)))
@@ -102,9 +166,11 @@
 ;; Execute all queued mapping transformations onto the Datashell's data,
 ;; then reduces the data with the given function.
 (define (ds-reduce-func afunc acc ds)
-  (if (procedure-arity-includes? afunc 2)
-      (foldl afunc acc (ds))
-      (error 'ds-reduce "Invalid reduce function, must take 2 arguments")))
+  (cond [(not (procedure-arity-includes? afunc 2))
+         (error 'ds-reduce "Invalid reduce function, must take 2 arguments")]
+        [(not (Datashell? ds))
+         (error 'ds-reduce "Invalid second argument, should be a Datashell")]
+        [else (foldl afunc acc (ds))]))
   
 ;; (Datashell [Listof Any] 
 (struct Datashell (dataset op) #:transparent #:property prop:procedure ds-collect)
--- test/tests.rkt
+++ test/tests.rkt
@@ -1,8 +1,13 @@
 #lang s-exp "../lib/racket-spark-light.rkt"
 
-(define x (mk-datashell '(12 2)))
-(define xy (ds-map add1 x))
-(define xz (ds-map (lambda (i) (+ 1 i)) x))
+(require (for-syntax syntax/parse))
+
+(save-ds x (mk-datashell '(12 2)))
+(save-ds xy (ds-map add1 x))
+(save-ds xz (ds-map (lambda (i) (+ 1 i)) x))
+
+;; only works if the underlying identifier is a datashell
+(save-ds xz-clone xz)
 
 (define xy-res (ds-reduce (lambda (x y) (+ x y)) 0 xy))
 (define xz-res (ds-reduce + 0 xz))
@@ -10,7 +15,29 @@
 xy-res
 xz-res
 
+;; Ds-maps can be chained an arbitrary number of times
 (ds-reduce (lambda (r acc) (+ r acc)) 0
            (ds-map (lambda (y) (* 2 y))
                    (ds-map (lambda (y) (* 2 y))
                            (mk-datashell '(1 3)))))
+
+;; Incorrect datashell argument
+#;(ds-reduce (lambda (r) (+  r)) 0 2)
+;; Incorrect function argument, must be TFunc
+#;(ds-reduce (lambda (r) (+  r 1)) 0 (mk-datashell '(1 2 3)))
+
+;; Gives a static error, because save-ds can only be used at the top level
+#;(define (y)
+  (save-ds (mk-datashell '(10 20))))
+
+(define-syntax save-datashell
+  (syntax-parser
+    [(_ e ...)
+     #'(save-ds e ...)]))
+
+;; Gives a static error, because save-ds can only be used at the top level
+#;(define (y-mac)
+  (save-datashell (mk-datashell '(10 20))))
+
+;; Gives a dynamic error, must be a datashell, not a number
+#;(save-ds n 2) 
